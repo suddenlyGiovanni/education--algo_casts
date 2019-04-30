a5eb3761177681a90abd09b74d23293c4cc5b97b
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-04-30 12:18:20 +0200

    feat(anagrams): added a solution not relying on a lookup table

--- src/anagrams/index.ts
+++ src/anagrams/index.ts
@@ -8,20 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-interface ChrMap {
+interface ChrLookupTable {
   [key: string]: number
 }
 
-export function characterMap(str: string): ChrMap {
-  const characters: ChrMap = {}
-  for (const char of str) {
-    const _char = char.toString()
-    characters[_char] = characters[_char]++ || 1
+export function buildChrLookupTable(str: string): ChrLookupTable {
+  const chrLookupTable: ChrLookupTable = {}
+  for (const chr of str) {
+    const chrStr = chr.toString()
+    chrLookupTable[chrStr] = chrLookupTable[chrStr]++ || 1
   }
-  return characters
+  return chrLookupTable
 }
 
-export function isEqual(a: ChrMap, b: ChrMap): boolean {
+export function escapeStr(str: string): string {
+  return str.replace(/[^\w]/g, '').toLowerCase()
+}
+
+export function sortStr(str: string): string {
+  return str
+    .split('')
+    .sort()
+    .join('')
+}
+
+export function isEqual(a: ChrLookupTable, b: ChrLookupTable): boolean {
   if (a === b) return true // the map are equal by reference
   const propsA = Object.getOwnPropertyNames(a)
   const propsB = Object.getOwnPropertyNames(b)
@@ -35,10 +46,19 @@ export function isEqual(a: ChrMap, b: ChrMap): boolean {
   return true
 }
 
-export default function anagrams(stringA: string, stringB: string): boolean {
-  const escapeStr = (_str: string): string =>
-    _str.replace(/[^\w]/g, '').toLowerCase()
-  const strMapA = characterMap(escapeStr(stringA)) //?
-  const strMapB = characterMap(escapeStr(stringB)) //?
+export function anagrams(stringA: string, stringB: string): boolean {
+  const strMapA = buildChrLookupTable(escapeStr(stringA)) //?
+  const strMapB = buildChrLookupTable(escapeStr(stringB)) //?
   return isEqual(strMapA, strMapB)
 }
+
+/**
+ * a solution relying on comparing two strings
+ */
+export function anagrams2(stringA: string, stringB: string): boolean {
+  const strA = sortStr(escapeStr(stringA)) //?
+  const strB = sortStr(escapeStr(stringB)) //?
+  return strA === strB
+}
+
+export default anagrams2
